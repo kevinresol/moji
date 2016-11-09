da725fb6ed1ab5950ebc3fc4ba73efdb91e981d4
kevinresol <kevinresol@gmail.com>
2016-11-09 23:25:06 +0800

    Clean up and add some descriptions

--- sample/Game.hx
+++ sample/Game.hx
@@ -3,9 +3,14 @@ package;
 import moji.*;
 using tink.CoreApi;
 
+/**
+	A sample game to demonstrate the game engine.
+	This game happens on a NxN chess board, where N is defined by boardSize
+	The player starts at a random position and there is a goal spawned at another random position
+	The player needs to choose the direction to move until he reaches the goal
+**/
 class Game {
 	static function main() {
-		moji.Condition;
 		var boardSize = 5;
 		var player = Position.random(boardSize, boardSize);
 		var goal = Position.random(boardSize, boardSize);
