c59507923202e6654372a6c33c7a4316eb10112f
kevinresol <kevinresol@gmail.com>
2016-11-14 21:50:30 +0800

    Adjust API

--- src/moji/core/Condition.hx
+++ src/moji/core/Condition.hx
@@ -32,6 +32,20 @@ abstract Condition(ConditionObject) from ConditionObject to ConditionObject {
 	public static inline function ofLazyAsync(f:Void->Future<Bool>):Condition
 		return new LazyCondition(f);
 		
+	public static function combine(conds:Array<Condition>, f:Array<Bool>->Bool):Condition
+		return new ConditionCombine(conds, f);
+	
+	public static function andAll(conds:Array<Condition>):Condition
+		return new ConditionCombine(conds, function(bools) {
+			for(bool in bools) if(!bool) return false;
+			return true;
+		});
+		
+	public static function orAll(conds:Array<Condition>):Condition
+		return new ConditionCombine(conds, function(bools) {
+			for(bool in bools) if(bool) return true;
+			return false;
+		});
 }
 
 class SimpleCondition extends ConditionBase {
@@ -57,43 +71,30 @@ class LazyCondition extends ConditionBase {
 
 class ConditionMap extends ConditionBase {
 	var cond:Condition;
-	var f:Bool->Bool;
+	var ifTrue:Condition;
+	var ifFalse:Condition;
 	
-	public function new(cond, f) {
+	public function new(cond, ifTrue, ifFalse) {
 		this.cond = cond;
-		this.f = f;
+		this.ifTrue = ifTrue;
+		this.ifFalse = ifFalse;
 	}
 	
 	override function resolve():Future<Bool>
-		return cond.resolve().map(f);
-}
-
-class ConditionMapAsync extends ConditionBase {
-	var cond:Condition;
-	var f:Bool->Condition;
-	
-	public function new(cond, f) {
-		this.cond = cond;
-		this.f = f;
-	}
-	
-	override function resolve():Future<Bool>
-		return cond.resolve().flatMap(function(b) return f(b).resolve());
+		return cond.resolve().flatMap(function(b) return (b ? ifTrue : ifFalse).resolve());
 }
 
 class ConditionCombine extends ConditionBase {
-	var cond1:Condition;
-	var cond2:Condition;
-	var f:Bool->Bool->Bool;
+	var conds:Array<Condition>;
+	var f:Array<Bool>->Bool;
 	
-	public function new(cond1, cond2, f) {
-		this.cond1 = cond1;
-		this.cond2 = cond2;
+	public function new(conds, f) {
+		this.conds = conds;
 		this.f = f;
 	}
 	
 	override function resolve():Future<Bool>
-		return Future.ofMany([cond1.resolve(), cond1.resolve()]).map(function(v) return f(v[0], v[1]));
+		return Future.ofMany([for(c in conds) c.resolve()]).map(f);
 }
 
 class ConditionBase implements ConditionObject {
@@ -101,19 +102,16 @@ class ConditionBase implements ConditionObject {
 		return Future.sync(true);
 	
 	public function negate():Condition
-		return new ConditionMap(this, function(b) return !b);
+		return new ConditionMap(this, false, true);
 		
 	public function and(other:Condition):Condition
-		return new ConditionCombine(this, other, function(v1, v2) return v1 && v2);
+		return new ConditionCombine([this, other], function(b) return b[0] && b[1]);
 		
 	public function or(other:Condition):Condition
-		return new ConditionCombine(this, other, function(v1, v2) return v1 || v2);
+		return new ConditionCombine([this, other], function(b) return b[0] || b[1]);
 		
-	public function map(f:Bool->Bool):Condition
-		return new ConditionMap(this, f);
-		
-	public function flatMap(f:Bool->Condition):Condition
-		return new ConditionMapAsync(this, f);
+	public function map(ifTrue:Condition, ifFalse:Condition):Condition
+		return new ConditionMap(this, ifTrue, ifFalse);
 	
 	public function then<T>(ifTrue:Conditional<T>, ifFalse:Conditional<T>):Conditional<T>
 		return new Conditional(this, ifTrue, ifFalse);
@@ -125,7 +123,6 @@ interface ConditionObject {
 	function negate():Condition;
 	function and(other:Condition):Condition;
 	function or(other:Condition):Condition;
-	function map(f:Bool->Bool):Condition;
-	function flatMap(f:Bool->Condition):Condition;
+	function map(ifTrue:Condition, ifFalse:Condition):Condition;
 	function then<T>(ifTrue:Conditional<T>, ifFalse:Conditional<T>):Conditional<T>;
 }
